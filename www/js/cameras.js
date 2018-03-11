0e4e13f166655602e8ff161a1b22de42d10e059d
Ellery Newcomer <ellery-newcomer@utulsa.edu>
2018-03-10 16:23:22 -0800

    update camera feeds for cscore
    retain automatic reload of image stream, lose the bugginess

--- www/js/cameras.js
+++ www/js/cameras.js
@@ -1,252 +1,62 @@
 
-function CameraFrame(obj) {
-    this.selector = obj.selector;
-    this.noSignal = obj.noSignal;
-    this.lastLoaded = 0;
-    this.state = "no-signal";
-    this.imageNr = 0;
-    this.finished = [];
-    this.lastTried = Date.now();
-    this.camera = null;
-    var frame = this;
-
-    $(function() {
-        frame.hideStream();
-        setInterval(function() {
-            var now = Date.now();
-            if (frame.state === "signal" && (now - frame.lastLoaded) >= 5000 && !frame.camera.singleImage) {
-                frame.state = "no-signal";
-                frame.hideStream();
-            }
-            if (frame.state === "no-signal" && 
-                    (now - frame.lastTried) >= 5000 &&
-                frame.camera != null &&
-                !frame.camera.singleImage) {
-                frame.lastTried = Date.now();
-                frame.createImageLayer();
-            }
-        }, 1000);
-    });
-}
-
-CameraFrame.prototype.getSignalImg = function() {
-    return $(this.selector + " .signal");
-};
-
-CameraFrame.prototype.getNoSignalImg = function() {
-    return $(this.selector + " .no-signal");
-};
-
-CameraFrame.prototype.showStream = function() {
-    this.getSignalImg().show();
-    this.getNoSignalImg().hide();
-};
-
-CameraFrame.prototype.hideStream = function() {
-    this.getSignalImg().hide();
-    this.getNoSignalImg().show();
-};
-
-CameraFrame.prototype.clear = function() {
-    this.getSignalImg().remove();
-    this.finished = [];
-};
-
-CameraFrame.prototype.setCamera = function(camera) {
-    this.camera = camera;
-    this.clear();
-    this.showStream();
-    this.createImageLayer();
-};
-
-CameraFrame.prototype.cycleCamera = function() {
-	this.setCamera(this.camera.nextCamera);
-};
-
-CameraFrame.prototype.createImageLayer = function() {
-    var frame = this;
-    var img = new Image();
-    img.style.zIndex = -1;
-    img.onload = function() {
-        frame.showStream();
-        frame.imageOnload(this);
-    };
-    img.onerror = function() {
-        frame.hideStream();
-    }
-    img.src = this.camera.url + "&n=" + (++this.imageNr);
-    $(img).addClass("signal")
-    var webcam = $(this.selector);
-    webcam.get(0).insertBefore(img, webcam.firstChild);
-};
-
-CameraFrame.prototype.imageOnload = function(img) {
-    var frame = this;
-    frame.showStream();
-    frame.lastLoaded = Date.now();
-    frame.state = "signal";
-    img.style.zIndex = frame.imageNr;
-    while (1 < frame.finished.length) {
-        var del = frame.finished.shift();
-        del.parentNode.removeChild(del);
-    }
-    frame.finished.push(img);
-    if(!frame.paused && !frame.camera.singleImage) {
-        setTimeout(function() {
-            frame.createImageLayer();
-        }, 100);
-    }
-};
-
-function Camera(args) {
+function CscoreCamera(args) {
     this.url = args.url;
-    this.singleImage = args.singleImage;
-    this.elt = $("#" + this.eltId);
-    this.elt.html("<noscript><img src='" + this.url + "'/></noscript>");
+    this.selector = args.selector;
+    this.noSignal = args.noSignal;
+    var camera = this;
+    camera.lastFailed = false;
+    camera.setStream();
+    setInterval(function() {
+        $.ajax({
+            url: camera.url + "/settings.json",
+            dataType: 'json',
+            error: function(errorResponse) {
+                camera.lastFailed = true;
+                camera.setStream(camera.noSignal);
+            },
+            success: function() {
+                if(camera.lastFailed) {
+                    camera.setStream();
+                    camera.lastFailed = false;
+                }
+            }
+        });
+    }, 1000);
 }
 
-var frameL = new CameraFrame({
-    selector: "#webcam0_stream",
-    noSignal: "/img/indianfront.png"
-});
+CscoreCamera.prototype.getImg = function() {
+    return $(this.selector + " img:first");
+}
 
-var frameR = new CameraFrame({
-    selector: "#webcam1_stream",
-    noSignal: "/img/indianback.png",
-});
+CscoreCamera.prototype.setStream = function(src) {
+    if(src == null) {
+        var timestamp = (new Date()).getTime();
+        src = this.url + "/stream.mjpg?a="+timestamp;
+        this.getImg().removeClass("no-signal");
+        this.getImg().addClass("signal");
+    }else{
+        this.getImg().removeClass("signal");
+        this.getImg().addClass("no-signal");
+    }
+    this.getImg().attr('src', src);
+}
 
-var piurl = "http://marschmahlo";
 var rio_url = "http://10.32.23.2";
 
 var frontCamera;
-
 var altCamera;
 
-var structureCamera;
-
-var nullCamera;
-
-var structureMode = 8;
-
-function initCameras() {
-    return;
-    frontCamera = new Camera({
-		url: rio_url + ":1181/stream.mjpg",
-		singleImage: false,
-        nextCamera: null
+$(function () {
+    frontCamera = new CscoreCamera({
+		url: rio_url + ":1181",
+        selector: "#webcam0_stream",
+        noSignal: "/img/indianfront.png"
     });
 
-    altCamera = new Camera({
-        url: piurl + ":5800/?action=snapshot",
-        singleImage: false,
-        nextCamera: null
+    altCamera = new CscoreCamera({
+        url: rio_url + ":1182",
+        selector: "#webcam1_stream",
+        noSignal: "/img/indianfront.png"
     });
 
-    nullCamera = new Camera({
-        url: "/img/indianfront.png",
-        singleImage: true,
-        nextCamera: null
-    });
-
-	frontCamera.nextCamera = nullCamera;
-	altCamera.nextCamera = nullCamera;
-	nullCamera.nextCamera = frontCamera;
-    frameL.setCamera(frontCamera);
-    frameR.setCamera(altCamera);
-    $("#rightStructureModeContainer").hide();
-    $("#leftStructureModeContainer").hide();
-
-   $("#rightFrameCamera").change(function(){
-   	var value = $("#rightFrameCamera").val();
-   	if (value == "front") {
-            frameR.setCamera(frontCamera);
-   	} else if (value == "back") {
-            frameR.setCamera(altCamera);
-   	} else if (value == "structure") {
-            frameR.setCamera(structureCamera);
-   	}else if(value == "none") {
-            frameR.setCamera(nullCamera);
-        }
-
-        if(value == "structure") {
-            $("#rightStructureModeContainer").show();
-        }else{
-            $("#rightStructureModeContainer").hide();
-        }
-   });
-   
-   $("#webcam1_stream").click(function() {
-	   frameR.cycleCamera();
-	   syncCameraDropdown(frameR, $("#rightFrameCamera"));
-   });
-   $("#webcam0_stream").click(function() {
-	   frameL.cycleCamera();
-	   
-	   syncCameraDropdown(frameL, $("#leftFrameCamera"));
-   });
-   
-   $("#leftFrameCamera").change(function(){
-   	var value = $("#leftFrameCamera").val();
-   	if (value == "front") {
-            frameL.setCamera(frontCamera);
-   	} else if (value == "back") {
-            frameL.setCamera(altCamera);
-   	} else if (value == "structure") {
-            frameL.setCamera(structureCamera);  	
-        } else if (value == "none") {
-            frameL.setCamera(nullCamera);
-   	}
-
-        if(value == "structure") {
-            $("#leftStructureModeContainer").show();
-        }else{
-            $("#leftStructureModeContainer").hide();
-        }
-   });
-
-   $("#leftStructureMode").change(function() {
-       var value = parseInt($("#leftStructureMode").val());
-       if(!isNaN(value)) {
-           structureMode = value;
-       }
-       NetworkTables.putValue("/SmartDashboard/structureMode", structureMode);
-   });
-   $("#rightStructureMode").change(function() {
-       var value = parseInt($("#rightStructureMode").val());
-       if(!isNaN(value)) {
-           structureMode = value;
-       }
-       NetworkTables.putValue("/SmartDashboard/structureMode", structureMode);
-   });
-}
-
-$(function() {
-    $.ajax({
-        url: "/piurl", 
-        success: function(response) {
-			console.info("repno", response);
-			response = JSON.parse(response);
-            piurl = response.pi;
-			rio_url = response.rio;
-			console.info("rio: ", rio_url);
-			console.info("piurl:" , piurl);
-            initCameras();
-        },
-        error: function(errorResponse) {
-            initCameras();
-        }
-    });
 });
-
-
-function syncCameraDropdown(cameraFrame, dropdown) {
-	if(cameraFrame.camera == frontCamera) {
-		dropdown.val("front");
-	}else if(cameraFrame.camera == altCamera) {
-		dropdown.val("back");
-	}else if(cameraFrame.camera == structureCamera) {
-		dropdown.val("structure");
-	}else if(cameraFrame.camera == nullCamera) {
-		dropdown.val("none");
-	}
-}
